0f27337fbbdf35b36b9b6dbe1d227a87e7583a31
Hyeongseok Kim <hyeongseok@coconutsilo.com>
2024-09-19 11:04:20 +0900

    refactor: TabView 제거 (#2)
    - selectedTab 변수 활용해 페이지 변경하는 것으로 구성 변경

--- tyte/Views/HomeView.swift
+++ tyte/Views/HomeView.swift
@@ -3,11 +3,11 @@ import SwiftUI
 struct HomeView: View {
     @ObservedObject var viewModel: HomeViewModel
     
-    @State private var todoInput = ""
     @State private var showSortMenu = false
     @State private var sortOption = "마감 임박순"
     @State private var selectedTags: [String] = []
     
+    @State private var todos : [Todo] = []
     
     // @State 프로퍼트를 직접 초기화하려 할 경우, 버그 발생 우려 -> onAppear 수정자에 값 설정 로직 추가
     // SwiftUI 뷰는 값 구조체이며 @State 변경 시 새로운 인스턴스가 생성됨. 즉 뷰가 자주 재생성 된다.
@@ -19,63 +19,46 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            VStack(alignment:.leading, spacing: 0) {
-                VStack (spacing: 8) {
+            VStack(alignment: .leading, spacing: 0) {
+                VStack(spacing: 8) {
                     Text("안녕하세요. 김형석님")
                         .font(._subhead1)
                         .foregroundColor(.gray50)
-                        .frame(maxWidth: .infinity,alignment: .leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     
-                    ScrollView(.horizontal, showsIndicators: false){
-                        HStack{
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
                             SortMenuButton(viewModel: viewModel)
-                            
                             TagSelector(viewModel: viewModel)
                         }
                     }
-                    
                     TodoViewSelector(viewModel: viewModel)
                 }
                 .padding(.horizontal)
                 
-                TabView(selection: $viewModel.currentTab) {
-                    ScrollView {
-                        ForEach(viewModel.inProgressTodos.filter { todo in
-                            if viewModel.selectedTags.contains("default") {
-                                return todo.tagId == nil || (todo.tagId != nil && viewModel.selectedTags.contains(todo.tagId!.id))
-                            } else {
-                                return todo.tagId != nil && viewModel.selectedTags.contains(todo.tagId!.id)
-                            }
-                        }) { todo in
-                            TodoItemView(todo: todo, isHome: true) {_ in viewModel.toggleTodo(todo.id)}
+                ScrollView {
+                    ForEach(viewModel.currentTab==0 ?
+                            viewModel.inProgressTodos.filter { todo in
+                        if viewModel.selectedTags.contains("default") {
+                            return todo.tagId == nil || (todo.tagId != nil && viewModel.selectedTags.contains(todo.tagId!.id))
+                        } else {
+                            return todo.tagId != nil && viewModel.selectedTags.contains(todo.tagId!.id)
                         }
-                        Spacer().frame(maxWidth:.infinity, maxHeight:80)
-                    }
-                    .scrollIndicators(.hidden)
-                    .padding(.horizontal)
-                    .background(.gray10)
-                    .tag(0)
-                    
-                    ScrollView {
-                        ForEach(viewModel.completedTodos.filter { todo in
-                            if viewModel.selectedTags.contains("default") {
-                                return todo.tagId == nil || (todo.tagId != nil && viewModel.selectedTags.contains(todo.tagId!.id))
-                            } else {
-                                return todo.tagId != nil && viewModel.selectedTags.contains(todo.tagId!.id)
-                            }
-                        }) { todo in
-                            TodoItemView(todo: todo, isHome: true) {_ in viewModel.toggleTodo(todo.id)}
+                    } : viewModel.completedTodos.filter { todo in
+                        if viewModel.selectedTags.contains("default") {
+                            return todo.tagId == nil || (todo.tagId != nil && viewModel.selectedTags.contains(todo.tagId!.id))
+                        } else {
+                            return todo.tagId != nil && viewModel.selectedTags.contains(todo.tagId!.id)
                         }
-                        Spacer().frame(maxWidth:.infinity, maxHeight:80)
+                    }) { todo in
+                        TodoItemView(todo: todo, isHome: true) { _ in viewModel.toggleTodo(todo.id) }
                     }
-                    .scrollIndicators(.hidden)
-                    .padding(.horizontal)
-                    .background(.gray10)
-                    .tag(1)
+                    Spacer().frame(maxWidth: .infinity, maxHeight: 120)
                 }
+                .scrollIndicators(.hidden)
+                .padding(.horizontal)
                 .background(.gray10)
             }
         }
-        .background(.gray00)
     }
 }
