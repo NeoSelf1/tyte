f67edc7bc200c8da8cb34bd5adf9764f7943ed07
Hyeongseok Kim <hyeongseok@coconutsilo.com>
2024-09-20 14:25:02 +0900

    refactor: StatusBox 내부 선택된 날짜 추가 (#2)

--- tyte/Components/List/StatusBox.swift
+++ tyte/Components/List/StatusBox.swift
@@ -8,46 +8,71 @@
 import SwiftUI
 
 struct StatusBoxContent: View {
-    let balanceData:BalanceData
+    @ObservedObject var viewModel : ListViewModel
+    private var balanceData:BalanceData
+    
+    init( viewModel: ListViewModel ) {
+        self.viewModel = viewModel
+        if let index = viewModel.weekCalenderData.firstIndex(where: {
+            viewModel.selectedDate.apiFormat == $0.date
+        }){
+            balanceData = viewModel.weekCalenderData[index].balanceData
+        } else {
+            balanceData =
+            BalanceData(
+                title: "Todo가 없네요 :(",
+                message: "아래 + 버튼을 눌러 Todo를 추가해주세요",
+                balanceNum: 0)
+        }
+    }
     
     var body: some View {
         HStack(spacing: 12) {
             ZStack {
                 CircularProgressView(progress: Double(balanceData.balanceNum) / 100.0, color: balanceData.balanceNum.colorByBalanceData)
                     .frame(width: 64, height: 64)
+                
                 HStack (spacing:0) {
                     Text("\(balanceData.balanceNum)")
                         .font(._headline2)
                         .foregroundColor(balanceData.balanceNum.colorByBalanceData)
                         .contentTransition(.numericText(value: Double(balanceData.balanceNum)))
                         .animation(.snappy, value: Double(balanceData.balanceNum))
+                    
                     Text("%")
                         .font(._body3)
                         .foregroundColor(balanceData.balanceNum.colorByBalanceData)
                 }
             }
-                
+            .padding(.leading,4)
             
-            VStack(alignment: .leading, spacing: 4) {
+            VStack(alignment: .leading,spacing:0) {
+                Text(viewModel.selectedDate.formattedDate)
+                    .font(._body3)
+                    .foregroundStyle(.gray90)
+                    .padding(.bottom,4)
+                
                 Text(balanceData.title)
                     .font(._title)
                     .foregroundStyle(.gray90)
+                    .padding(.bottom,2)
+                
                 Text(balanceData.message)
                     .font(._body3)
                     .foregroundStyle(.gray50)
             }
-            
             Spacer()
         }
-        .padding(12)
+        .padding(10)
+        .frame(height:96)
         .background(.gray00)
         .clipShape(RoundedRectangle(cornerRadius: 8))
-        .shadow(color: .gray90.opacity(0.08), radius: 4)
+        .shadow(color: .gray90.opacity(0.08), radius: 2)
         .overlay(
             RoundedRectangle(cornerRadius: 8)
                 .stroke(.blue10, lineWidth: 1)
         )
-        .padding(1)
+        .padding(4)
 
     }
 }
