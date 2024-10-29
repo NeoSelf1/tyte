39817cd4f1b73e99015623575cfb8ddafb453776
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-10-29 21:19:21 +0900

    feat: AuthViewModel에서 SettingsViewModel 분리 (#16)

---
+++ tyte/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,71 @@
+//
+//  SettingsViewModel.swift
+//  tyte
+//
+//  Created by Neoself on 10/29/24.
+//
+
+import Foundation
+import Combine
+import AuthenticationServices
+import GoogleSignIn
+import SwiftUI
+
+class SettingsViewModel: ObservableObject {
+    let appState = AppState.shared
+    
+    private let authService: AuthService
+
+    init(authService: AuthService = AuthService.shared) {
+        self.authService = authService
+    }
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    func deleteAccount() {
+        if let userEmail = APIManager.shared.getUserEmail() {
+            authService.deleteAccount(userEmail)
+                .receive(on: DispatchQueue.main)
+                .sink { [weak self] completion in
+                    if case .failure(let error) = completion {
+                        self?.appState.currentToast = .error(error.localizedDescription)
+                    }
+                } receiveValue: { [weak self] deleteResponse in
+                    self?.appState.isLoggedIn = false
+                }
+                .store(in: &cancellables)
+        }
+    }
+    
+    func logout() {
+        do {
+            if let savedEmail = UserDefaults.standard.string(forKey: "lastLoggedInEmail")  {
+                try KeychainManager.delete(service: APIConstants.tokenService,
+                                           account: savedEmail)
+            }
+            
+            GIDSignIn.sharedInstance.signOut()
+            UserDefaults.standard.removeObject(forKey: "lastLoggedInEmail")
+            appState.isLoggedIn = false
+            clearAllUserData()
+        } catch {
+            self.appState.currentToast = .error(error.localizedDescription)
+        }
+    }
+    
+    private func clearAllUserData() {
+        let defaults = UserDefaults.standard
+        let allKeys = defaults.dictionaryRepresentation().keys
+        allKeys.forEach { key in
+            if key.starts(with: "com.yourapp.") { // 앱 관련 키에 대해서만 삭제
+                defaults.removeObject(forKey: key)
+            }
+        }
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: APIConstants.tokenService
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
