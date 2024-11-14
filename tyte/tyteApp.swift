5d2be3415d63ec16d60bf354be0fe771e948dd19
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-11-14 17:14:45 +0900

    fix: CustomPop 배경색상 변경 (#6)

--- tyte/tyteApp.swift
+++ tyte/tyteApp.swift
@@ -27,7 +27,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     }
 }
 
-
+//MARK: - Toast, Popup 상태관리 및 온보딩 vs 메인화면 관리
 struct ContentView: View {
     @EnvironmentObject var appState: AppState
     @State private var isToastPresent = false
