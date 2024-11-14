d83bacff911e9c760ad355af808f1d589cc2bd60
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-11-14 16:41:35 +0900

    feat: PopupType, ToastType 열거형 정의 (#21)
    - delete: 기존 ToastType 제거

--- tyte/AppState.swift
+++ tyte/AppState.swift
@@ -3,8 +3,31 @@ import Foundation
 class AppState: ObservableObject {
     @Published var isLoggedIn: Bool = UserDefaultsManager.shared.isLoggedIn
     @Published private(set) var isGuestMode: Bool = false
+    @Published private(set) var currentPopup: PopupData?
+    @Published private(set) var currentToast: ToastData?
     
     static let shared = AppState()
     
-    @Published var currentToast: ToastType?
+    private init() {}
+    
+    //MARK: - 메서드
+    func showToast(_ type: ToastType, action: (() -> Void)? = nil) {
+        currentToast = ToastData(type: type, action: action)
+    }
+    
+    func changeGuestMode(_ state: Bool) {
+        isGuestMode = state
+    }
+    
+    func removeToast(){
+        currentToast = nil
+    }
+    
+    func showPopup(type: PopupType, action: @escaping () -> Void) {
+        currentPopup = PopupData(type: type, action: action)
+    }
+    
+    func removePopup(){
+        currentPopup = nil
+    }
 }
