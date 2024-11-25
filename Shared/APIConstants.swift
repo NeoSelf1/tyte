e0fdde05b37b6da190d2487dda5347875abd0934
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-11-25 13:49:22 +0900

    build: v1.0.1 배포 (#26)

--- Shared/APIConstants.swift
+++ Shared/APIConstants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct APIConstants {
-    static let isServerDevelopment = true
-    static let isUserDevelopment = true
+    static let isServerDevelopment = false
+    static let isUserDevelopment = false
     static let baseUrl = isServerDevelopment ? "http://localhost:8080/api" : "http://43.201.140.227:8080/api"
 }
