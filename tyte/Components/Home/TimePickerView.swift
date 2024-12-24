62b6fce6df47b4d5f0f5ddfd3edef7a6a6166861
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-12-25 03:47:22 +0900

    refactor: TodoEditBottomSheet 디자인 리뉴얼 (#44)

--- tyte/Components/Home/TimePickerView.swift
+++ tyte/Components/Home/TimePickerView.swift
@@ -1,8 +1,45 @@
+//
+//  TimePickerView.swift
+//  tyte
+//
+//  Created by Neoself on 12/24/24.
+//
 import SwiftUI
+
+struct TimePickerView: View {
+    @Binding var estimatedTime: Int
+    
+    var body: some View {
+        VStack {
+            NavigationBar(title: "소요시간 수정")
+            
+            Spacer()
+            
+            Text(estimatedTime.formattedDuration)
+                .font(._subhead1)
+                .foregroundColor(.gray90)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .contentTransition(.numericText(value: Double(estimatedTime)))
+                .animation(.snappy, value: estimatedTime)
+            
+            TimeWheelPicker(value: Binding<CGFloat>(
+                get: { CGFloat(estimatedTime) },
+                set: { newValue in
+                    estimatedTime = Int(round(newValue))
+                }
+            ))
+            
+            Spacer()
+        }
+        .background(.gray00)
+        
+        .navigationBarBackButtonHidden(true)
+    }
+}
 
 struct TimeWheelPicker: View {
     private let count:Int = 8
-    private let spacing:CGFloat = 20
+    private let spacing:CGFloat = 30
     private let multiplier:Int = 10
     private let steps:Int = 6
     
@@ -24,16 +61,15 @@ struct TimeWheelPicker: View {
                             
                             Divider()
                                 .background(remainder == 0 ? .gray90 : .gray50)
-                                .frame(width: 2, height: remainder == 0 ? 20 : 10, alignment: .center)
-                                .frame(maxHeight: 20, alignment: .bottom)
+                                .frame(width: 4, height: remainder == 0 ? 80 : 50, alignment: .center)
+                                .frame(maxHeight: 80, alignment: .top)
                                 .overlay(alignment: .bottom) {
                                     if remainder == 0 {
                                         Text(((index / steps) * multiplier * 6).formattedDuration)
-                                            .font(.caption)
-                                            .fontWeight(.semibold)
+                                            .font(._body1)
                                             .foregroundColor(.gray60)
                                             .fixedSize()
-                                            .offset(y: 20)
+                                            .offset(y: 32)
                                     }
                                 }
                         }
@@ -51,8 +87,8 @@ struct TimeWheelPicker: View {
                 }
                 .overlay(alignment: .center) {
                     Rectangle()
-                        .frame(width: 1, height: 40)
-                        .padding(.bottom, 20)
+                        .frame(width: 1, height: 80)
+                        .padding(.bottom, 50)
                 }
                 .safeAreaPadding(.horizontal, horizontalPadding)
                 .onAppear {
@@ -63,5 +99,14 @@ struct TimeWheelPicker: View {
                 }
             }
         }
+        .frame(height: 280)
     }
 }
+
+//#Preview("TimePickerView") {
+////    NavigationStack {
+//        TimePickerView(estimatedTime: .constant(30))
+//            .frame(maxHeight: 520)
+//            .border(.gray50)
+////    }
+//}
