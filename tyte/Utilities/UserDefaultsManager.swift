5ac07cc05945680263304816e09acf6bd896e4f5
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2025-01-12 18:20:56 +0900

    refactor: UserDefaultsManager 위젯타겟 제거, WidgetManager 클래스 의존성 주입 (#54)

--- tyte/Utilities/UserDefaultsManager.swift
+++ tyte/Utilities/UserDefaultsManager.swift
@@ -28,21 +28,22 @@ final class UserDefaultsManager {
     static let shared = UserDefaultsManager()
     
     private let defaults: UserDefaults!
+    private let widgetManager: WidgetManager
     
-    private init(defaults: UserDefaults = .standard) {
-        let groupDefaults = UserDefaults(suiteName: UserDefaultsConfiguration.suiteName)!
-        self.defaults = groupDefaults
+    private init(
+        defaults: UserDefaults = .standard,
+        widgetManager: WidgetManager = .shared
+    ) {
+        self.defaults = defaults
+        self.widgetManager = widgetManager
     }
     
-    // MARK: - 속성들 캡슐화와 데이터 무결성을 보장하기 위해 private(set) 사용 ->  클래스 내부에서만 수정 가능
-    // 읽기는 public, 쓰기는 private
     private(set) var isLoggedIn: Bool {
         get { defaults.bool(forKey: UserDefaultsConfiguration.Keys.isLoggedIn) }
         set {
             defaults.set(newValue, forKey: UserDefaultsConfiguration.Keys.isLoggedIn)
             AppState.shared.isLoggedIn = newValue // TODO: 다른 반응형 프레임워크로 환경객체 접근하는 방안 모색 필요
-            WidgetCenter.shared.reloadTimelines(ofKind: "CalendarWidget")
-            WidgetCenter.shared.reloadTimelines(ofKind: "TodoListWidget")
+            widgetManager.updateWidget(.all)
         }
     }
     
